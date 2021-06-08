c12e0a60bff234133390a9b7c61c3ee8c9830b9b
ARINDAM ROY <arindamroy044@gmail.com>
2021-06-08 21:26:53 +0530

    Update Reverse.java

--- Array/Reverse.java
+++ Array/Reverse.java
@@ -1,7 +1,6 @@
-public class Array1 {
-
-	static void rvereseArray(int arr[],
-					int start, int end)
+public class Array1 
+{
+	static void rvereseArray(int arr[],int start, int end)
 	{
 		int temp;
 		
@@ -15,18 +14,16 @@ public class Array1 {
 		}
 	}	
 	
-	static void printArray(int arr[],
-							int size)
+	static void printArray(int arr[], int size)
 	{
 		for (int i = 0; i < size; i++)
 			System.out.print(arr[i] + " ");
 		
 		System.out.println();
 	}
-
-
-	public static void main(String args[]) {
-		
+	
+	public static void main(String args[]) 
+	{	
 		int arr[] = {1, 2, 3, 4, 5, 6};
 		printArray(arr, 6);
 		rvereseArray(arr, 0, 5);
